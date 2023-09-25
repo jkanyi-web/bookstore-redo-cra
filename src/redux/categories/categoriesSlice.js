2ad48760b778617741d896c3eaed4f967b0579f1
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-09-25 17:36:06 +0300

    minor changes

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -9,7 +9,7 @@ export const categoriesSlice = createSlice({
   initialState,
   reducers: {
     checkStatus: (state, action) => {
-      state.categories = action.payload === 'Under Construction' ? 'Under Construction' : state.categories;
+      state.categories = action.payload === 'Under construction' ? 'Under construction' : state.categories;
     },
   },
 });
